f3f67ab6a331a82b926853a4262a0a2131b3e79e
hailey96 <hailey96@vt.edu>
2018-06-04 12:22:49 -0400

    Updated overlay file to adjust for hourly and daily

--- HARP-2018/model_gage_overlay.R
+++ HARP-2018/model_gage_overlay.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(scales)
 
 #update to location of config.local.private file
-config_file <- "C:\\Users\\nrf46657\\Desktop\\VAHydro Development\\GitHub\\hydro-tools\\"
+config_file <- "C:\\Users\\HaileyMae\\Documents\\GitHub\\hydro-tools"
 
 #----------------------------------------------------------------------------------------
 #load functions
@@ -14,8 +14,8 @@ source(paste(repo_location,"hydro-tools\\USGS\\usgs_gage_functions.R", sep = "")
 #----------------------------------------------------------------------------------------
 
 # SPECIFY GAGE AND MODEL SEGMENT OF INTEREST
-gageID <- '03171000'
-model_segment <- 'OD2_8840_9020_0111'
+gageID <- '02073000'
+model_segment <- 'OD3_8630_8720_0111'
 
 #RETRIEVE MODEL DATA
 URL_model_hourly <-paste("http://deq2.bse.vt.edu/p532c-sova/wdm/river/p532cal_062211/stream/",model_segment,".csv",sep="")
@@ -38,3 +38,36 @@ plt <- ggplot(data = model_hourly,aes(date, flow))+
 filename <- paste(gageID,"__",model_segment,".png", sep="")
 ggsave(file=filename, path = save_directory, width=8, height=6)
 
+gaugeplot<-gage_data[19816:27851,]
+gaugeplot$flowalt<-numeric(length(gaugeplot$Date))
+p=1
+for (p in 1:length(gaugeplot)) {
+  gaugeplot$flowalt<-gaugeplot$Flow
+  p<-p+1
+}
+  
+
+j<-1
+k<-24
+h<-1
+i<-1
+modelplot<-data.frame(matrix(nrow=1,ncol=1))
+for (i in 1:length(gaugeplot$Date)){
+  dailyflow<-sum(model_hourly[j:k,2])
+      modelplot[i,2]<-gaugeplot$Date[h]
+      modelplot[i,3]<-dailyflow
+      modelplot[i,4]<-dailyflow*0.5041667
+      j<-j+24
+      k<-k+24
+      h=h+1
+      i=i+1
+}
+
+
+par(mfrow=c(1,1))
+plot(gaugeplot$Date, gaugeplot$flowalt, type='l', col="red")
+lines(modelplot$V2, modelplot$V4, col="blue", type='l')
+
+par(mfrow=c(1,2))
+plot(gaugeplot$Date, gaugeplot$flowalt, type='l', col="red", ylim= c(0,10000))
+plot(modelplot$V2, modelplot$V4, col="blue", type='l', ylim= c(0,10000))
