a9660f9e7777780fb42221c7b047b7a778b8ad26
hailey96 <hailey96@vt.edu>
2018-06-08 12:54:12 -0400

    Created a code to compare area weighted averge daily flows of model and USGS. Fixed model-gage overlay code to pull dates on its own

--- HARP-2018/model_gage_overlay.R
+++ HARP-2018/model_gage_overlay.R
@@ -1,6 +1,6 @@
 #The following code is used to convert hourly data from the model to daily data and compare the daily data of the model with the daily USGS data
-#The majority of the code below was provided by Joey, Hailey made adjustments starting below the gg2plot command
-#The only portions of the code that need to be changed to compare the model to USGS is the gageID, model segment, gageplot range and the ylim range
+#The majority of the code below was provided by Joey, Hailey made adjustments starting below the ggplot command
+#The only portions of the code that need to be changed to compare the model to USGS is the gageID & model segment(be sure to leave the 0111 at the end of the segement)
 
 library(lubridate)
 library(ggplot2)
@@ -18,8 +18,8 @@ source(paste(repo_location,"hydro-tools\\USGS\\usgs_gage_functions.R", sep = "")
 #----------------------------------------------------------------------------------------
 
 # SPECIFY GAGE AND MODEL SEGMENT OF INTEREST
-gageID <- '02077500'
-model_segment <- 'OD2_8830_8710_0111'
+gageID <- '03531500'
+model_segment <- 'TU3_9230_9260_0111'
 
 #RETRIEVE MODEL DATA
 URL_model_hourly <-paste("http://deq2.bse.vt.edu/p532c-sova/wdm/river/p532cal_062211/stream/",model_segment,".csv",sep="")
@@ -36,7 +36,7 @@ gage_data <- clean_historic(gage_data)
 
 #PLOT DATA using ggplot
 #plt <- ggplot(data = model_hourly,aes(date, flow))+
-              #geom_point(data = model_hourly, aes(date, flow))
+#              geom_point(data = model_hourly, aes(date, flow))
 
 
 #filename <- paste(gageID,"__",model_segment,".png", sep="")
@@ -46,40 +46,36 @@ gage_data <- clean_historic(gage_data)
 #-----------------------------------------------------------------------------------------------
 #Hailey's modifications start below
 
-#Shorten total gage data to just data that the model has access to 1984-01-01 to 2005-12-31
-#This number must be altered by hand. Click on gage_data and search 1984-01 copy the row number (probably in the 1000s) into the number before the :
-#Then click on gage_data again and serach 2005-12-31 and copy and paste that row number into the place behind the :
-gageplot<-gage_data[8128:14721,]
+#Shorten total gage data to just data that the model has access to 1984-10-01 to 2005-09-30
+startdate<-(which(gage_data$Date == '1984-10-01'))
+enddate<-(which(gage_data$Date=='2005-09-30'))
+gageplot<-gage_data[startdate:enddate,]
 
 #Counters/Initalizing Variables
 j<-1      #Start at the first hour (0:00)
 k<-24     #Stop after 1 day has completed (23:00)
-h<-1      #Counter for the date
 i<-1      #Counter for the loop
-modelplot<-data.frame(matrix(nrow=1,ncol=1))     #Create empty vector for daily model data to go
+modelplot<-data.frame(matrix(nrow=1,ncol=1)) #Create empty vector for daily model data to go
 
 #For loop to do daily data calculations
 for (i in 1:length(gageplot$Date)){         #Run the loop for as many dates as the gage data has
   dailyflow<-sum(model_hourly[j:k,2])       #Calculate the model daily flow by summing the the 24 rows of the second column of the model_hourly data
-      modelplot[i,2]<-gageplot$Date[h]      #Recreate the date column for model data (Used gageplot data for ease of formatting model_hourly could have been used but extra steps would be required to remove the hour from the date column)
-      modelplot[i,3]<-dailyflow             #Assign the daily flow in ac-ft/day to one column
-      modelplot[i,4]<-dailyflow*0.5041655   #Multiply the model data by 0.5041655 to go from ac-ft/day to cfs 
+      modelplot[i,1]<-dailyflow             #Assign the daily flow in ac-ft/day to one column
+      modelplot[i,2]<-dailyflow*0.5041655   #Multiply the model data by 0.5041655 to go from ac-ft/day to cfs 
+      modelplot[i,3]<-gageplot$Date[i]      #Recreate the date column for model data (Used gageplot data for ease of formatting model_hourly could have been used but extra steps would be required to remove the hour from the date column)
       j<-j+24                               #Add 24 hours to the first time value
       k<-k+24                               #Add 24 hours to the last time value
-      h=h+1                                 #Add 1 day to the date
       i=i+1                                 #Move the loop forward 1 count
 }
-names(modelplot)<-c('Purposely Blank', 'Date', 'DailyFlow(ac-ft/day)', 'DailyFlow(cfs)')
+names(modelplot)<-c('DailyFlow(ac-ft/day)', 'DailyFlow(cfs)','Date') #Rename columns of modelplot data
 
 
 #Plot To Compare USGS and Model Data
-par(mfrow=c(1,1))                                                                                                    #Both data sets on one graph
+gage_legend<- paste('Gage',gageID)                            #Creates legend title for gage
+model_legend<-paste('Model River Seg. \n', model_segment)     #Creates legend title for model
+
+par(mfrow=c(1,1))                                                                                #Both data sets on one graph
 par(cex=1.1)
-plot(gageplot$Date, gageplot$Flow, type='l', col="red", xlab='Date', ylab='Flow (cfs)', main='Daily Discharge Data', lwd=1.5) #USGS gage data
-lines(modelplot$Date, modelplot$`DailyFlow(cfs)`, col="blue", type='l', lwd=1.5)                                                              #Model data
-legend ("topright", legend=c("USGS Gage", "Model Output"), col=c('red','blue'), lty=1, bty='n')
-
-
-par(mfrow=c(1,2))                                                                                                                              #Two seperate graphs side by side ylim will need to be adjusted accordingly but to better compare should be the same for both graphs
-plot(gageplot$Date, gageplot$Flow, type='l', col="red", ylim= c(0,5000), xlab='Date', ylab='Flow (cfs)', main='Daily Discharge of USGS Gage')  #USGS gage data
-plot(modelplot$Date, modelplot$`DailyFlow(cfs)`, col="blue", type='l', ylim= c(0,5000), xlab='Date', ylab='Flow (cfs)', main='Daily Discharge of Model Data')  #Model data
+plot(gageplot$Date, gageplot$Flow, type='l', col="red", xlab='Date', ylab='Flow (cfs)', lwd=1.5) #USGS gage data
+lines(modelplot$Date, modelplot$`DailyFlow(cfs)`, col="blue", type='l', lwd=1.5)                 #Model data
+legend ("topright", legend=c(gage_legend,model_legend), col=c('red','blue'), lty=1, bty='n')
