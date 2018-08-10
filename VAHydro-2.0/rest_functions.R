ac2da014f201ca6cef30b573941a60702434bbdd
Burgholzer <robert.burgholzer@deq.virginia.gov>
2018-08-10 09:53:56 -0400

    Set stringsasfactors = false in getProperty, cast propvalue to numeric and pid to integer for cleaner behavior and updates.

--- VAHydro-2.0/rest_functions.R
+++ VAHydro-2.0/rest_functions.R
@@ -236,8 +236,8 @@ getProperty <- function(inputs, base_url, prop){
     }
     inputs$varid = varid
   }
-  # now, verify that we have a proper varid
-  if (is.null(inputs$varid)) {
+  # now, verify that we have either a proper varid OR a propname
+  if (is.null(inputs$varid) & is.null(inputs$propname)) {
     # we were sent a bad variable id so we should return FALSE
     return(FALSE)
   }
@@ -245,10 +245,11 @@ getProperty <- function(inputs, base_url, prop){
   pbody = list(
     bundle = 'dh_properties',
     featureid = inputs$featureid,
-    entity_type = inputs$entity_type,
-    varid = inputs$varid
+    entity_type = inputs$entity_type 
   );
-  
+  if (!is.null(inputs$varid)) {
+    pbody$varid = inputs$varid
+  }
   if (!is.null(inputs$propcode)) {
     pbody$propcode = inputs$propcode
   }
@@ -296,22 +297,24 @@ getProperty <- function(inputs, base_url, prop){
     i <- 1
     for (i in 1:length(prop_cont$list)) {
       
-      prop_i <- data.frame("proptext" = if (is.null(prop_cont$list[[i]]$proptext)){""} else {prop_cont$list[[i]]$proptext},
-                           "pid" = if (is.null(prop_cont$list[[i]]$pid)){""} else {prop_cont$list[[i]]$pid},
-                           "propname" = if (is.null(prop_cont$list[[i]]$propname)){""} else {prop_cont$list[[i]]$propname},
-                           "propvalue" = if (is.null(prop_cont$list[[i]]$propvalue)){""} else {prop_cont$list[[i]]$propvalue},
-                           "propcode" = if (is.null(prop_cont$list[[i]]$propcode)){""} else {prop_cont$list[[i]]$propcode},
-                           "startdate" = if (is.null(prop_cont$list[[i]]$startdate)){""} else {prop_cont$list[[i]]$startdate},
-                           "enddate" = if (is.null(prop_cont$list[[i]]$enddate)){""} else {prop_cont$list[[i]]$enddate},
-                           "featureid" = if (is.null(prop_cont$list[[i]]$featureid)){""} else {prop_cont$list[[i]]$featureid},
-                           "modified" = if (is.null(prop_cont$list[[i]]$modified)){""} else {prop_cont$list[[i]]$modified},
-                           "entity_type" = if (is.null(prop_cont$list[[i]]$entity_type)){""} else {prop_cont$list[[i]]$entity_type},
-                           "bundle" = if (is.null(prop_cont$list[[i]]$bundle)){""} else {prop_cont$list[[i]]$bundle},
-                           "varid" = if (is.null(prop_cont$list[[i]]$varid)){""} else {prop_cont$list[[i]]$varid},
-                           "uid" = if (is.null(prop_cont$list[[i]]$uid)){""} else {prop_cont$list[[i]]$uid},
-                           "vid" = if (is.null(prop_cont$list[[i]]$vid)){""} else {prop_cont$list[[i]]$vid},
-                           "status" = if (is.null(prop_cont$list[[i]]$status)){""} else {prop_cont$list[[i]]$status},
-                           "module" = if (is.null(prop_cont$list[[i]]$module)){""} else {prop_cont$list[[i]]$module}
+      prop_i <- data.frame(
+        "proptext" = if (is.null(prop_cont$list[[i]]$proptext)){""} else {prop_cont$list[[i]]$proptext},
+        "pid" = if (is.null(prop_cont$list[[i]]$pid)){""} else {as.integer(prop_cont$list[[i]]$pid)},
+        "propname" = if (is.null(prop_cont$list[[i]]$propname)){""} else {prop_cont$list[[i]]$propname},
+        "propvalue" = if (is.null(prop_cont$list[[i]]$propvalue)){""} else {as.numeric(prop_cont$list[[i]]$propvalue)},
+        "propcode" = if (is.null(prop_cont$list[[i]]$propcode)){""} else {prop_cont$list[[i]]$propcode},
+        "startdate" = if (is.null(prop_cont$list[[i]]$startdate)){""} else {prop_cont$list[[i]]$startdate},
+        "enddate" = if (is.null(prop_cont$list[[i]]$enddate)){""} else {prop_cont$list[[i]]$enddate},
+        "featureid" = if (is.null(prop_cont$list[[i]]$featureid)){""} else {prop_cont$list[[i]]$featureid},
+        "modified" = if (is.null(prop_cont$list[[i]]$modified)){""} else {prop_cont$list[[i]]$modified},
+        "entity_type" = if (is.null(prop_cont$list[[i]]$entity_type)){""} else {prop_cont$list[[i]]$entity_type},
+        "bundle" = if (is.null(prop_cont$list[[i]]$bundle)){""} else {prop_cont$list[[i]]$bundle},
+        "varid" = if (is.null(prop_cont$list[[i]]$varid)){""} else {prop_cont$list[[i]]$varid},
+        "uid" = if (is.null(prop_cont$list[[i]]$uid)){""} else {prop_cont$list[[i]]$uid},
+        "vid" = if (is.null(prop_cont$list[[i]]$vid)){""} else {prop_cont$list[[i]]$vid},
+        "status" = if (is.null(prop_cont$list[[i]]$status)){""} else {prop_cont$list[[i]]$status},
+        "module" = if (is.null(prop_cont$list[[i]]$module)){""} else {prop_cont$list[[i]]$module},
+        stringsAsFactors=FALSE
       )
       prop  <- rbind(prop, prop_i)
     }
@@ -383,6 +386,9 @@ postProperty <- function(inputs,fxn_locations,base_url,prop){
     
   } else if (length(dataframe$pid) == 1){
     print("Single Property Exists, Updating...")
+    print(paste("Posting", pbody$varid )) 
+    print(pbody) 
+    #pbody$pid = pid
     prop <- PUT(paste(base_url,"/dh_properties/",pid,sep=""), 
                 add_headers(HTTP_X_CSRF_TOKEN = token),
                 body = pbody,
