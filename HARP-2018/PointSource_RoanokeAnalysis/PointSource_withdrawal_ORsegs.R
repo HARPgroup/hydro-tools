c8ebaddc765320b231fb8ffb5c3f842a22064712
Kelsey Reitz <rkels97@vt.edu>
2019-02-13 18:01:52 -0500

    added code to give comparison csv's for different scenarios

--- HARP-2018/PointSource_RoanokeAnalysis/PointSource_withdrawal_ORsegs.R
+++ HARP-2018/PointSource_RoanokeAnalysis/PointSource_withdrawal_ORsegs.R
@@ -64,45 +64,85 @@ colnames(mergesegs) <- c("rownum", "year", "month", "day", "hour", "8020_8130", 
 
 
 
-## Begin actual consolidation from days >>> year  --------------------
-# code does not work beyond this currently
+## Begin analysis of point sources and where / what year they are in  --------------------
+# 
+# convert hourly to daily: referencing the data_downloader R script from HARP 2018 DEQ_Model_vs_USGS_comparison
 
-means <- data.frame() #create an empty storage dataframe
-years<- data.frame(unique(mergesegs$year))
- 
-## Search for whether data exists in each of these years (get number of unique wdraw values) -----------
-# the data frame uniquecheck will return a value of NA if there are no point source withdrawals for that year 
-uniquecheck <- data.frame()
-k <- 1 # case study 1996 
 
-for (k in 1:nrow(years)){
-datayear <- subset(mergesegs, year==years[k,1])
+# IMPORTING AND EXPORTING MODEL DATA ----------------------------------------------------
 
-# unique(datayear$`8020_8130`)
-# unique(datayear$`8130_7900`)
-uniquecheck[k,1]<- years[k,1]
-uniquecheck[k,2]<- length(unique(datayear$`8020_8130`))
-uniquecheck[k,3]<- length(unique(datayear$`8130_7900`))
+# Splitting the River Segment string into each segment name
 
-if (uniquecheck[k,2]==1){
-  uniquecheck[k,2]<- (unique(datayear$`8020_8130`))
-  if (uniquecheck[k,3]==1){
-    uniquecheck[k,3]<- (unique(datayear$`8130_7900`))
-  }
+
+
+RivSegStr <- seg_8130_7900        # not analyzing upstream, just down
+start.date <- "1984-01-01"
+end.date <- "2005-12-31"
+
+model_days <- length(seq(as.Date(start.date):as.Date(end.date)))
+
+#Reads data into data frame "ovols", exports each seg's data
+
+# Downloading and exporting hourly model data
+model_hourly <- RivSegStr 
   
-} else if (uniquecheck[k,3]==1){
-  uniquecheck[k,3]<- (unique(datayear$`8130_7900`))
-}
-}
-colnames(uniquecheck) <- c("year", "8020_8130", "8130_7900")
-uniquecheck[uniquecheck == 0] <- NA
-
-print(uniquecheck)
-
-
-
-
-# unique(OR2_8130_7900$ps)
-# unique(OR2_8020_8130$ps)
+# Converting hourly to daily data and exporting daily data
+# model_hourly <- model_hourly[-1,]
+#model_hourly$V1 <- trimws(model_hourly$V1, which = "both")
+colnames(model_hourly) <- c("year","month","day","hour","ovol", "rownum")
+model_hourly$date <- as.Date(paste0(model_hourly$year,"-",model_hourly$month,"-",model_hourly$day))
+model_daily <- aggregate(model_hourly$ovol, list(model_hourly$date), FUN = sum)
+colnames(model_daily) <- c("date","mod.flow")
+#write.csv(model_daily, file = paste0(container, "/data/new_(updated)_data/raw_data/model_data/daily_data/",RivSeg," - Daily Raw Data.csv"))
   
-  
+# convert from ac-ft/day to cfs
+model_daily$mod.flow <- model_daily$mod.flow * 0.504167
+model_daily <- subset(model_daily, year(model_daily$date)==2002)
+
+# different ways that monthly flow may be interpreted by the model
+
+# mean of monthly flows
+model_monthly_bymean <- aggregate(model_daily$mod.flow, list(month(model_daily$date)), FUN = mean)
+colnames(model_monthly_bymean) <- c("month","mod.flow")
+
+# day one of the month
+model_monthly_byday1 <- data.frame(subset(model_daily, day(model_daily$date)==1))
+colnames(model_monthly_byday1) <- c("month","mod.flow")
+
+
+
+# pull in withdrawal data from wayside and spring hollow --------------------
+mon_wayside <- read.csv("wayside_mon.csv", stringsAsFactors = FALSE)
+mon_spholla <- read.csv("springhol_mon.csv", stringsAsFactors = FALSE) 
+
+mon_wayside$Date <- as.Date(mon_wayside$Date)
+mon_spholla$Date <- as.Date(mon_spholla$Date)
+mon_wayside$Value <- as.numeric(gsub(",", "",mon_wayside$Value))
+mon_spholla$Value <- as.numeric(gsub(",", "",mon_spholla$Value))
+
+
+mon_wayside <- subset(mon_wayside, varkey=="wd_mgm" & year(mon_wayside$Date)==2002)
+mon_spholla <- subset(mon_spholla, varkey=="wd_mgm" & year(mon_spholla$Date)==2002)
+
+vahydro <- data.frame(mon_wayside$Date, mon_wayside$Value, mon_spholla$Value)
+vahydro <- vahydro[order(vahydro$mon_wayside.Date), ]
+
+
+
+
+## compare wayside, spring hollow, and MEAN monthly model data -------------
+comparison <- data.frame(vahydro[,1:3], model_monthly_bymean$mod.flow)
+colnames(comparison)<- c("date", "wayside", "sphol", "model")
+
+comparison$summed <- comparison$wayside + comparison$sphol
+comparison$error <- 100*(comparison$model - comparison$summed) / (comparison$model)
+write.csv(comparison, "segs_vs_meanmonthly.csv")
+
+
+## compare wayside, spring hollow, and day1 monthly model data -------------
+comparisonday <- data.frame(vahydro[,1:3], model_monthly_byday1$mod.flow)
+colnames(comparisonday)<- c("date", "wayside", "sphol", "model")
+
+comparisonday$summed <- comparisonday$wayside + comparisonday$sphol
+comparisonday$error <- 100*(comparisonday$model - comparisonday$summed) / (comparisonday$model)
+write.csv(comparisonday,"segs_vs_day1monthly.csv")
