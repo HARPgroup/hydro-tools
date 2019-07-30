a2735a26854b19469d60cc25761928e153963ada
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-07-30 13:29:15 -0400

    Kable latex defined column width with column_spec() and scale_down format example.

--- OWS/AnnualReporting/kable_latext_table.R
+++ OWS/AnnualReporting/kable_latext_table.R
@@ -6,5 +6,9 @@ sheet <- "PWS"
 
 data <- read_excel(paste(folder,source,sep=""),sheet)
 kable(data, "latex", booktabs = T) %>%
-  kable_styling(latex_options = "striped")
+  kable_styling(latex_options = "striped") %>%
+  column_spec(1, width = "0.75in")%>%
+  column_spec(2:11, width = "0.5in")
 
+kable(data, "latex", booktabs = T) %>%
+  kable_styling(latex_options = c("striped", "scale_down"))
