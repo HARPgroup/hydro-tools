77dc9982b3d029fed810b6a74a5afc3ab00e385f
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2019-10-30 10:14:05 -0400

    download_web_image.R example

--- examples/download_web_image.R
+++ examples/download_web_image.R
@@ -1,9 +1,9 @@
 library('png') #required for readPNG()
 
 image_url <- 'https://deq1.bse.vt.edu/drought/state/images/maps/imageMapFile157244242118784.png'
-download.file(image_url,'image.png', mode = 'wb')
+download.file(image_url,'image.png', mode = 'wb') #downloads image to current directory
 
-
+#Display image in R:
 image_nativeRaster <- readPNG("image.png",native=TRUE)
 plot(0:1,0:1,type="n",ann=FALSE,axes=FALSE)
 rasterImage(image_nativeRaster,0,0,1,1)
