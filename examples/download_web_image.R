eeed8d028b26d05c60c4b7f54584429d48361117
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2020-01-21 09:49:28 -0500

    QA R script added which pulls in allocation view exports, groups by facility, and allocates groundwater share by MP, then sums the GW for each facility - includes missing link QA for GWP facilities that do not have a wsp link - and sql_base_snapshot_QA_demand script which generates has_permit table that gets joined to facility_join

--- examples/download_web_image.R
+++ examples/download_web_image.R
@@ -9,3 +9,10 @@ plot(0:1,0:1,type="n",ann=FALSE,axes=FALSE)
 rasterImage(image_nativeRaster,0,0,1,1)
 
 
+df <- file.info(list.files("https://deq1.bse.vt.edu/drought/state/images/maps/", full.names = T))
+rownames(df)[which.max(df$mtime)]
+
+tmpshot <- fileSnapshot("https://deq1.bse.vt.edu/drought/state/images/maps")
+rownames(tmpshot$info[which.max(tmpshot$info$mtime),])
+
+list.files("https://deq1.bse.vt.edu/drought/")
