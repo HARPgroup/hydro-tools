a8d6e5a079f3fad470f8366b24aaf03f0e7ab7f9
jdoe <nabrahams@vt.edu>
2020-07-29 16:01:52 -0400

    updates to app and elfgen_algorithm

--- HARP-2020/Elfgen/elfgen_algorithm.R
+++ HARP-2020/Elfgen/elfgen_algorithm.R
@@ -13,9 +13,9 @@ source(paste(basepath,'auth.PRIVATE',sep='/'))
 save_directory <-  "/var/www/html/data/proj3/out"
 
 
-hydroid <- '59760' #'59760'   #'59776'
+hydroid <- '59776' #'59760'   #'59776'
 huc_level<- 'huc8'
-flow_metric<- 'erom_q0001e_aug'
+flow_metric<- 'erom_q0001e_mean'
 flow_reduction_pct<-10
 site_comparison <- paste('http://deq1.bse.vt.edu/d.dh/dh-feature-contained-within-export', hydroid, "watershed", sep = '/')
 containing_watersheds <- read.csv(file=site_comparison, header=TRUE, sep=",")
@@ -52,14 +52,15 @@ y.sampres <- 'species'
 datasite <- 'http://deq2.bse.vt.edu/d.dh'
 
 # elfdata_vahydro() function for retrieving data from VAHydro
-watershed.df <- elfdata_vahydro(watershed.code,watershed.bundle,watershed.ftype,x.metric,y.metric,y.sampres,datasite)
+watershed.df <- elfdata_vahydro(watershed.code,watershed.bundle,watershed.ftype,
+                                x.metric,y.metric,y.sampres,datasite)
 
 # clean_vahydro() function for cleaning data by removing any stations where the ratio of DA:Q is greater than 1000, also aggregates to the maximum richness value at each flow value
 watershed.df <- clean_vahydro(watershed.df)
 
 elf_quantile <- 0.80
-breakpt <- bkpt_pwit("watershed.df" = watershed.df, "quantile" = elf_quantile, "blo" = 100, "bhi" = 1000)  
-
+breakpt <- bkpt_pwit("watershed.df" = watershed.df, "quantile" = elf_quantile, 
+                     "blo" = 100, "bhi" = 1000)  
 
 elf <- elfgen("watershed.df" = watershed.df,
               "quantile" = elf_quantile,
@@ -98,21 +99,26 @@ colnames(watershed.df)[1] <- "x_var"
 colnames(watershed.df)[2] <- "y_var"
 colnames(watershed.df)[3] <- "watershed"
 
-data_below_breakpt <- watershed.df[!(watershed.df$x_var > breakpt),]
+# data_below_breakpt <- watershed.df[!(watershed.df$x_var > breakpt),]
 
-# UPPER SUBSET
-upper_quant_data <- rq(y_var ~ log(x_var),data = data_below_breakpt, tau = 0.8)
+# # UPPER SUBSET
+# upper_quant_data <- rq(y_var ~ log(x_var), data = data_below_breakpt, tau = 0.8)
+# 
+# 
+# newy <- c(log(data_below_breakpt$x_var)*coef(upper_quant_data)[2]+coef(upper_quant_data)[1])
+# upper_quant <- subset(data_below_breakpt, data_below_breakpt$y_var > newy)
 
+up<-data.frame(elf$stats$quantile)
 
-newy <- c(log(data_below_breakpt$x_var)*coef(upper_quant_data)[2]+coef(upper_quant_data)[1])
-upper_quant <- subset(data_below_breakpt, data_below_breakpt$y_var > newy)
 
 upper<-upper_quant
 
-upper.lm <-lm(y_var~log(x_var), data=upper)
+upper.lm <-lm(y_var~(log(x_var)), data=upper)
 
-predict<-data.frame(predict(upper.lm, newdata=data.frame(x_var=mean_intake), interval = 'confidence'))
-
+predict<-data.frame(predict(upper.lm, 
+                            newdata=data.frame(x_var=mean_intake), 
+                            interval = 'confidence', 
+                            level=0.95))
 
 lwr<-as.numeric(predict$lwr)
 upr<-as.numeric(predict$upr)
@@ -124,6 +130,23 @@ species_richness
 
 percent_error<-((fit-species_richness)/species_richness)*100
 percent_error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
