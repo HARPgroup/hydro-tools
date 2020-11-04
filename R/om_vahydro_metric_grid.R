da8d50102b10588f46e6800b78129e425fc70fa9
Burgholzer <robert.burgholzer@deq.virginia.gov>
2020-11-04 13:28:46 -0500

    added a function to retrieve results and put in a table

---
+++ R/om_vahydro_metric_grid.R
@@ -0,0 +1,58 @@
+om_vahydro_metric_grid <- function (
+  metric,
+  runids,
+  featureid = 'all',
+  entity_type = 'dh_feature',
+  bundle = 'watershed',
+  ftype = 'vahydro',
+  model_version = 'vahydro-1.0',
+  base_url = "http://deq2.bse.vt.edu/d.dh/entity-model-prop-level-export"
+) {
+  alldata = NULL
+  for (i in 1:nrow(runids)) {
+    runinfo = runids[i,]
+    if (is.data.frame((runinfo))) {
+      print("Found info")
+      print(runinfo)
+      # user is passing in other params in data frame format
+      runid = as.character(runinfo$runid)
+      if (!is.null(runinfo$model_version)) model_version = as.character(runinfo$model_version)
+      if (!is.null(runinfo$metric)) metric = as.character(runinfo$metric)
+      if (!is.null(runinfo$runlabel)) runlabel = as.character(runinfo$runlabel)
+    } else {
+      # only runid is passed in
+      runid = runinfo
+      runlabel = runid
+    }
+    runlabel <- str_replace_all(runlabel, '-', '_')
+    runlabel <- str_replace_all(runlabel, ' ', '_')
+    params <- paste(featureid,entity_type,bundle,ftype,model_version, runid, metric,sep="/")
+    url <- paste(base_url,params,sep="/")
+    print(paste("retrieving ", url))
+    rawdata <- read.csv(url)
+    if (is.null(alldata) ) {
+      alldata = sqldf(
+        paste(
+          "select a.pid, a.propname, a.hydrocode, a.featureid, a.riverseg, a.attribute_value as ",
+          runlabel, 
+          "from rawdata as a "
+        )
+      )
+    } else {
+      mergeq = paste(
+        "select a.*, b.attribute_value as ",
+        runlabel, 
+        "from alldata as a 
+        left outer join rawdata as b 
+        on (
+          a.featureid = b.featureid
+        )"
+      )
+      message(mergeq)
+      alldata = sqldf(
+        mergeq
+      )
+    }
+  }
+  return(alldata)
+}
