00455a483b6fbd3f4df90aa0c2a8d73440db9d5a
kylewlowe <kylel@vt.edu>
2021-04-28 14:21:34 -0400

    updating cia functions

--- R/cia_utils.R
+++ R/cia_utils.R
@@ -47,6 +47,8 @@ CIA_data <- function(riv_seg, runid1, runid2, flow_metric, AllSegList){
   
   #deleting sub_length column bc all values are not in length
   cia_data$sub_length <- NULL
+  #deleting riv_seg column bc it is a duplicate
+  cia_data$riv_seg <- NULL
   
   #creating percent change columns
   #calculating Percent change values for mean annual flow and inputed metric flow
@@ -66,70 +68,6 @@ CIA_data <- function(riv_seg, runid1, runid2, flow_metric, AllSegList){
     cia_data$Metric_change <- '+'
   }
   
-  # #Adding length segments together to form river mile (distance from headwater) column
-  # i <- 1
-  # while (i <= nrow(cia_data)) {
-  #   
-  #   river_length <- c()
-  #   
-  #   # Loop creates vector of current segment and upstream segment lengths
-  #   for (n in 1:i) {
-  #     n_length <- as.numeric(cia_data$length[n])
-  #     river_length <- c(river_length, n_length)
-  #   }
-  #   # Makes length column to total length to segment from start of river
-  #   cia_data$mile[i] <- sum(river_length)
-  #   
-  #   i <- i + 1
-  # }
-  # 
-  # # Creating a river mile column
-  # for (i in 1:(length(cia_data$mile))){
-  #   if(i == 1){
-  #     cia_data$rmile[i] <- cia_data$mile[length(cia_data$mile)]
-  #   }
-  #   else{
-  #     cia_data$rmile[i] <- cia_data$mile[length(cia_data$mile)] - cia_data$mile[i-1]
-  #   }
-  # }
-  # 
-  # # Calculating Percent change values for mean annual flow and inputed metric flow
-  # cia_data$Qout_pc <- ((cia_data$Qout_2 - cia_data$Qout_1)/cia_data$Qout_1)*100
-  # cia_data$metric_pc <- ((cia_data$Metric_2 - cia_data$Metric_1)/cia_data$Metric_1)*100
-  # 
-  # # Must make seg list numbered for x axis on graphs with bars, could be used in table companion
-  # cia_data$seglist = as.numeric(c(1:nrow(cia_data)))
-  # 
-  # # Now lets graph using mean baseflow
-  # # Pre setting y axes max
-  # if (max(cia_data$Qout_1 >= cia_data$Qout_2)) {
-  #   y_prim <- c(0,max(cia_data$Qout_1) + 100)
-  # } else {
-  #   y_prim <- c(0, max(cia_data$Qout_2) + 100)
-  # }
-  # y_sec <-  c(min(cia_data$Qout_pc) - 2, max(cia_data$Qout_pc) + 2)
-  # 
-  # coeff <- max(y_sec) / max(y_prim)
-  # cia_data$Qout_pc_graph <- cia_data$Qout_pc/coeff
-  # 
-  # # Pre setting y axes max (Do Not Need If No More ggplot)
-  # if (max(cia_data$Metric_1 >= cia_data$Metric_2)) {
-  #   y_prim2 <- c(0,max(cia_data$Metric_1) + 100)
-  # } else {
-  #   y_prim2 <- c(0, max(cia_data$Metric_2) + 100)
-  # }
-  # y_sec2 <-  c(min(cia_data$metric_pc) - 2, max(cia_data$metric_pc) + 2)
-  # 
-  # coeff2 <- max(y_sec2) / max(y_prim2)
-  # cia_data$metric_pc_graph <- cia_data$metric_pc/coeff2
-  
-  
-  
-  #return(list(cia_data,
-  #           y_prim,
-  #          y_prim2,
-  #         coeff,
-  #        coeff2))
-  
   return(cia_data)
 }
+
