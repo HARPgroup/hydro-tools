c83174c0fac93a2331425ec4f2234ace3e090616
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2021-05-19 16:15:29 -0400

    Push VWP Location Map Example

--- GIS_functions/base.layers.R
+++ GIS_functions/base.layers.R
@@ -1,15 +1,15 @@
 library(sqldf)
 
-
-base.layers <- function(map_layers,extent = data.frame(x = c(-84, -75),y = c(35.25, 40.6))){
+#PROCESS BASE LAYERS
+base.layers <- function(baselayers,extent = data.frame(x = c(-84, -75),y = c(35.25, 40.6))){
   
-  # # LOAD MAP LAYERS FROM THE map_layers LIST 
-  STATES <- map_layers[[which(names(map_layers) == "STATES")]]
-  MinorBasins.csv <- map_layers[[which(names(map_layers) == "MinorBasins.csv")]]
-  #RSeg.csv <- map_layers[[which(names(map_layers) == "RSeg.csv")]]
-  MajorRivers.csv <- map_layers[[which(names(map_layers) == "MajorRivers.csv")]]
-  # fips.csv <- map_layers[[which(names(map_layers) == "fips.csv")]]
-  WBDF <- map_layers[[which(names(map_layers) == "WBDF")]]
+  # # LOAD MAP LAYERS FROM THE baselayers LIST 
+  STATES <- baselayers[[which(names(baselayers) == "STATES")]]
+  MinorBasins.csv <- baselayers[[which(names(baselayers) == "MinorBasins.csv")]]
+  #RSeg.csv <- baselayers[[which(names(baselayers) == "RSeg.csv")]]
+  MajorRivers.csv <- baselayers[[which(names(baselayers) == "MajorRivers.csv")]]
+  # fips.csv <- baselayers[[which(names(baselayers) == "fips.csv")]]
+  WBDF <- baselayers[[which(names(baselayers) == "WBDF")]]
   
   
   print(extent)
@@ -308,4 +308,135 @@ base.layers <- function(map_layers,extent = data.frame(x = c(-84, -75),y = c(35.
   )
   return(baselayers.gg)
   
+}
+
+#LOAD BASE LAYERS
+load_MapLayers <- function(site,localpath = tempdir()){
+  library(ggplot2)
+  library(rgeos)
+  library(ggsn)
+  library(rgdal) # needed for readOGR()
+  library(dplyr) # needed for case_when()
+  library(sf) # needed for st_read()
+  library(sqldf)
+  library(kableExtra)
+  library(viridis) #magma
+  #library(wicket) #wkt_centroid() (package was removed from CRAN on 1/27/21)
+  library(wellknown) #Replaces the wicket package for wkt_centroid()
+  library(cowplot) #plot static legend
+  library(magick) #plot static legend
+  library(ggrepel) #needed for geom_text_repel()
+  library(ggmap) #used for get_stamenmap, get_map
+  
+  #DOWNLOAD STATES AND MINOR BASIN LAYERS DIRECT FROM GITHUB
+  print(paste("DOWNLOADING STATES AND MINOR BASIN LAYERS DIRECT FROM GITHUB...",sep=""))  
+  
+  if(!exists("STATES")) {  
+    STATES_item <- "https://raw.githubusercontent.com/HARPgroup/HARParchive/master/GIS_layers/STATES.tsv"
+    STATES_filename <- "STATES.tsv"
+    #file downloaded into local directory, as long as file exists it will not be re-downloaded
+    if (file.exists(paste(localpath, STATES_filename, sep = '/')) == FALSE) {
+      print(paste("__DOWNLOADING STATES LAYER", sep = ''))
+      destfile <- paste(localpath,STATES_filename,sep="\\")
+      download.file(STATES_item, destfile = destfile, method = "libcurl")
+    } else {
+      print(paste("__STATES LAYER PREVIOUSLY DOWNLOADED", sep = ''))
+    }
+    #read csv from local directory
+    print(paste("__LOADING STATES LAYER...", sep = ''))
+    STATES <- read.csv(file=paste(localpath,STATES_filename,sep="\\"), header=TRUE, sep="\t")
+    print(paste("__COMPLETE!", sep = ''))
+  }  
+  
+  if(!exists("MinorBasins.csv")) {  
+    MinorBasins.csv_item <- "https://raw.githubusercontent.com/HARPgroup/HARParchive/master/GIS_layers/MinorBasins.csv"
+    MinorBasins.csv_filename <- "MinorBasins.csv.tsv"
+    #file downloaded into local directory, as long as file exists it will not be re-downloaded
+    if (file.exists(paste(localpath, MinorBasins.csv_filename, sep = '/')) == FALSE) {
+      print(paste("__DOWNLOADING MinorBasins.csv LAYER", sep = ''))
+      destfile <- paste(localpath,MinorBasins.csv_filename,sep="\\")
+      download.file(MinorBasins.csv_item, destfile = destfile, method = "libcurl")
+    } else {
+      print(paste("__MinorBasins.csv LAYER PREVIOUSLY DOWNLOADED", sep = ''))
+    }
+    #read csv from local directory
+    print(paste("__LOADING MinorBasins.csv LAYER...", sep = ''))
+    MinorBasins.csv <- read.csv(file=paste(localpath,MinorBasins.csv_filename,sep="\\"), header=TRUE, sep=",")
+    print(paste("__COMPLETE!", sep = ''))  
+  }
+  
+  #DOWNLOAD RSEG LAYER DIRECT FROM VAHYDRO
+  if(!exists("RSeg.csv")) {  
+    print(paste("DOWNLOADING RSEG LAYER DIRECT FROM VAHYDRO...",sep=""))
+    RSeg.csv_item <- paste(site,"/vahydro_riversegs_export",sep="")
+    RSeg.csv_filename <- "RSeg.csv"
+    #file downloaded into local directory, as long as file exists it will not be re-downloaded
+    if (file.exists(paste(localpath, RSeg.csv_filename, sep = '/')) == FALSE) {
+      print(paste("__DOWNLOADING RSeg.csv LAYER", sep = ''))
+      destfile <- paste(localpath,RSeg.csv_filename,sep="\\")
+      download.file(RSeg.csv_item, destfile = destfile, method = "libcurl")
+    } else {
+      print(paste("__RSeg.csv LAYER PREVIOUSLY DOWNLOADED", sep = ''))
+    }
+    #read csv from local directory
+    print(paste("__LOADING RSeg.csv LAYER...", sep = ''))
+    RSeg.csv <- read.csv(file=paste(localpath,RSeg.csv_filename,sep="\\"), header=TRUE, sep=",")
+    print(paste("__COMPLETE!", sep = ''))  
+  }
+  
+  #DOWNLOAD MAJORRIVERS LAYER DIRECT FROM GITHUB
+  if(!exists("MajorRivers.csv")) {  
+    print(paste("DOWNLOADING MAJORRIVERS LAYER DIRECT FROM GITHUB...",sep=""))
+    MajorRivers.csv_item <- "https://raw.githubusercontent.com/HARPgroup/HARParchive/master/GIS_layers/MajorRivers.csv"
+    MajorRivers.csv_filename <- "MajorRivers.csv"
+    #file downloaded into local directory, as long as file exists it will not be re-downloaded
+    if (file.exists(paste(localpath, MajorRivers.csv_filename, sep = '/')) == FALSE) {
+      print(paste("__DOWNLOADING MajorRivers.csv LAYER", sep = ''))
+      destfile <- paste(localpath,MajorRivers.csv_filename,sep="\\")
+      download.file(MajorRivers.csv_item, destfile = destfile, method = "libcurl")
+    } else {
+      print(paste("__MajorRivers.csv LAYER PREVIOUSLY DOWNLOADED", sep = ''))
+    }
+    #read csv from local directory
+    print(paste("__LOADING MajorRivers.csv LAYER...", sep = ''))
+    MajorRivers.csv <- read.csv(file=paste(localpath,MajorRivers.csv_filename,sep="\\"), header=TRUE, sep=",")
+    print(paste("__COMPLETE!", sep = ''))  
+  }
+  
+  #DOWNLOAD FIPS LAYER DIRECT FROM VAHYDRO
+  if(!exists("fips.csv")) {  
+    print(paste("DOWNLOADING FIPS LAYER DIRECT FROM VAHYDRO...",sep=""))
+    fips.csv_item <- paste(site,"/usafips_centroid_export",sep="")
+    fips.csv_filename <- "fips.csv"
+    #file downloaded into local directory, as long as file exists it will not be re-downloaded
+    if (file.exists(paste(localpath, fips.csv_filename, sep = '/')) == FALSE) {
+      print(paste("__DOWNLOADING fips.csv LAYER", sep = ''))
+      destfile <- paste(localpath,fips.csv_filename,sep="\\")
+      download.file(fips.csv_item, destfile = destfile, method = "libcurl")
+    } else {
+      print(paste("__fips.csv LAYER PREVIOUSLY DOWNLOADED", sep = ''))
+    }
+    #read csv from local directory
+    print(paste("__LOADING fips.csv LAYER...", sep = ''))
+    fips.csv <- read.csv(file=paste(localpath,fips.csv_filename,sep="\\"), header=TRUE, sep=",")
+    print(paste("__COMPLETE!", sep = '')) 
+  }
+  
+  #DOWNLOAD RESERVOIR LAYER FROM LOCAL REPO
+  if(!exists("WBDF")) { 
+    print(paste("__LOADING RESERVOIR LAYER FROM LOCAL REPO...",sep=""))
+    WBDF <- read.table(file=paste(github_location,"HARPArchive/GIS_layers","WBDF.csv",sep="/"), header=TRUE, sep=",")
+  }
+  
+  #LOAD ANY ADDITIONL MAPPING FUNCTIONS
+  source(paste(vahydro_location,"R/wsp/wsp2020/FoundationDataset/geo_summaries/mb.extent.R",sep = '/'))
+  
+  layers <- list("STATES" = STATES, 
+                 "MinorBasins.csv" = MinorBasins.csv,
+                 "RSeg.csv" = RSeg.csv,
+                 "MajorRivers.csv" = MajorRivers.csv,
+                 "fips.csv" = fips.csv,
+                 "WBDF" = WBDF
+  )
+  return(layers)
 }
---
+++ examples/rest_2.0_example.R
@@ -0,0 +1,22 @@
+library(pander);
+library(httr);
+library(zoo);
+library(hydrotools);
+
+site <- "http://deq2.bse.vt.edu/d.alpha"    #Specify the site of interest, either d.bet OR d.dh
+omsite = site
+
+# Load Libraries
+basepath='/var/www/R'
+source('/var/www/R/config.R')
+options(timeout=1200) # set timeout to twice default level to avoid abort due to high traffic
+
+
+################################################################################################
+source(paste(hydro_tools_location,'/VAHydro-2.0/rest_functions.R',sep=""))
+
+??vahydro_auth_read()
+
+################################################################################################
+################################################################################################
+
