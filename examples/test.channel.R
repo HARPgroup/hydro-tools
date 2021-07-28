e45c6524aa91af4b52ecc62b3b3219aedcb005bb
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-07-28 11:02:08 -0400

    channel props

---
+++ examples/test.channel.R
@@ -0,0 +1,74 @@
+# install_github("HARPGroup/hydro-tools", force=TRUE)
+library("hydrotools")
+ds <- RomDataSource$new("http://deq1.bse.vt.edu/d.dh", 'restws_admin')
+ds$get_token()
+hid <- 68123 
+
+map1 <- RomProperty$new(
+  ds, list(
+    propcode="vahydro-1.0", 
+    entity_type="dh_feature",
+    featureid=hid
+  ),
+  TRUE
+)
+model <- ds$get_prop(
+  config = list(
+    featureid = 68123, 
+    entity_type='dh_feature', 
+    propcode = 'vahydro-1.0'
+  ),
+  return_type = 'object'
+)
+
+# use new json
+wshed_obj_url <- paste(json_obj_url, model$pid, sep="/")
+wshed_model_info <- om_auth_read(wshed_obj_url, token,  "text/json", "")
+
+# All channel data
+# use model parameter retrieval routine
+# Facility analysis
+df <- data.frame(
+  'model_version' = c('vahydro-1.0',  'vahydro-1.0'),
+  'runid' = c('local_channel','local_channel'),
+  'metric' = c('drainage_area', 'province'),
+  'runlabel' = c('drainage_area', 'province')
+)
+channel_data <- om_vahydro_metric_grid(
+  metric, df, 'all', 'dh_feature', 'watershed','all',
+  "vahydro-1.0","http://deq1.bse.vt.edu/d.dh/entity-model-prop-level-export"
+)
+
+# 1 = appalachian plateau
+# 2 = Valley and Ridge
+# 3 = Piedmont 
+# 4 = Coastal Plain
+
+coeffs <- rbind(
+  list(
+    hc = 2.030,he = 0.2310,bfc = 12.175,bfe = 0.4711,bc = 5.389,be = 0.5349,n = 0.036
+  ),
+  # valley & Ridge
+  list(
+    hc = 1.43,he = 0.2830,bfc = 13.216,fe = 0.4532,bc = 4.667,be = 0.5489,n = 0.038
+  ),
+  # Piedmont
+  list(
+    hc = 2.137,he = 0.2561,bfc = 14.135,bfe = 0.4111,bc = 6.393,be = 0.4604,n = 0.095
+  ),
+  # Coastal Plain
+  list(cco$bc *channel$drainage_area ^ cco$be
+    hc = 2.820, he = 0.2000, bfc = 15.791, bfe = 0.3758, bc = 6.440, be = 0.4442, n = 0.040
+  )
+)
+
+
+for (i in nrow(channel_data)) {
+  channel <- channel_data[i,]
+  prov <- channel$province
+  cco <- coeffs[prov,]
+  h = cco$hc * channel$drainage_area ^ cco$he
+  bf = cco$bfc * channel$drainage_area  ^ cco$bfe
+  b = cco$bc *channel$drainage_area ^ cco$be
+  z = 0.5 * (bf - b) / h; 
+}
