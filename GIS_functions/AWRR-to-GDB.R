6ce18e063aea16dee1a69ea5055d447d3f40ff49
Mineva <Janet.Mineva@deq.virginia.gov>
2021-10-12 11:14:21 -0400

    save wsp areas and drought regions to shapefile format

--- GIS_functions/AWRR-to-GDB.R
+++ GIS_functions/AWRR-to-GDB.R
@@ -7,6 +7,7 @@ library('dplyr')
 library('tidyr')
 library(maptools)
 library("beepr")
+library(sf)
 # USER INPUTS #########################################################################
 #WKT_layer <- read.csv('C:/Users/nrf46657/Desktop/VAHydro Development/GitHub/hydro-tools/GIS_LAYERS/MinorBasins.csv')
 
@@ -266,3 +267,68 @@ print(paste0("PROCESS COMPLETE: ",syear," - ",eyear," MONTHLY GIS LAYER"))
 # 4) Export Data Table to .gdb for all layers
 # 5) Clip the layers to the VA extent
 # Final .gdb should have 6 clipped layers and 6 complete data tables
+
+## 2015 WSP Areas to Shapefile ################################################
+#PULL IN WSP Region List from local file
+wsp_regions <- read.csv(paste(folder,"ows_wsp_regions_wkt.csv",sep=""))
+wsp_regions$region_name <- as.character(wsp_regions$region_name)
+
+wsp_df <- sqldf('SELECT *  
+         FROM wsp_regions
+         WHERE geom IS NOT NULL
+         AND "FIPS.Code" NOT LIKE "3%"')
+
+#convert WKT into an sf object
+wsp.sf <- st_as_sf(wsp_df, wkt = 'geom')
+
+#convert sf object to SpatialPolygonDataframe
+wsp.spdf <- as(wsp.sf, "Spatial")
+
+# assign the CRS
+crs(wsp.spdf) <- "+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs 
+                 +ellps=WGS84 +towgs84=0,0,0"
+# visually confirm that it can plot/worked!
+plot(wsp.spdf, 
+     main="Map of WSP Areas", border = wsp.spdf$region_featureid)
+str(wsp.spdf)
+# write a shapefile
+writeOGR(wsp.spdf, "C:/Users/maf95834/Documents/shp_output","ows_wsp_regions", driver="ESRI Shapefile", overwrite_layer = T)
+
+## Drought Regions to Shapefile ################################################
+#PULL IN drought regions from local file
+fips_df <-sqldf(paste('SELECT *,fips_geom AS geom,
+                          CASE
+                            WHEN fips_code IN (51191,51167,51169,51173,51520,51185,51720,51105,51027,51051,51195) THEN "',color_list[1],'"
+                            WHEN fips_code IN (51025,51081,51053,51620,51111,51135,51149,51175,51181,51183,51595) THEN "',color_list[2],'"
+                            WHEN fips_code IN (51131,51001) THEN "',color_list[3],'"
+                            WHEN fips_code IN (51049,51075,51085,51087,51009,51570,51125,51145,51147,51041,51670,51680,51730,51760,51007,51065,51003,51011,51029,51540) THEN "',color_list[4],'"
+                            WHEN fips_code IN (51021,51197,51035,51063,51071,51121,51155,51640,51077,51750) THEN "',color_list[5],'"
+                            WHEN fips_code IN (51057,51073,51099,51103,51097,51101,51133,51159,51115,51119,51033,51193) THEN "',color_list[6],'"
+                            WHEN fips_code IN (51047,51109,51113,51137,51157,51177,51179,51630,51079) THEN "',color_list[7],'"
+                            WHEN fips_code IN (51013,51059,51061,51610,51153,51600,51107,51683,51685,51510) THEN "',color_list[8],'"
+                            WHEN fips_code IN (51031,51037,51067,51083,51590,51089,51161,51690,51019,51770,51775,51143,51117,51515,51141) THEN "',color_list[9],'"
+                            WHEN fips_code IN (51820,51840,51171,51015,51043,51165,51660,51187,51790,51139,51069) THEN "',color_list[10],'"
+                            WHEN fips_code IN (51093,51550,51800,51810,51710,51740) THEN "',color_list[11],'"
+                            WHEN fips_code IN (51045,51163,51678,51530,51005,51091,51580,51023,51017) THEN "',color_list[12],'"
+                            WHEN fips_code IN (51830,51036,51095,51127,51650,51199,51735,51700) THEN "',color_list[13],'"
+                            ELSE "white"
+                          END AS col
+                        FROM fips_csv 
+                        WHERE fips_code NOT LIKE "3%"',sep="")) #EXCLUDE NC LOCALITIES
+
+#convert WKT into an sf object
+fips.sf <- st_as_sf(fips_df, wkt = 'geom')
+
+#convert sf object to SpatialPolygonDataframe
+fips.spdf <- as(fips.sf, "Spatial")
+
+# assign the CRS
+crs(fips.spdf) <- "+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs 
+                 +ellps=WGS84 +towgs84=0,0,0"
+# visually confirm that it can plot/worked!
+plot(fips.spdf, 
+     main="Map of Drought Areas", border = fips.spdf$col)
+str(fips.spdf)
+# write a shapefile
+writeOGR(fips.spdf, "C:/Users/maf95834/Documents/shp_output","ows_drought_regions", driver="ESRI Shapefile", overwrite_layer = T)
+
