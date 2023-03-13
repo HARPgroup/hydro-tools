388a60f4e8edfec5391cb654186d704b63552143
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-03-13 14:19:52 -0400

    remove config.R since calling function should use it

--- GIS_functions/model_geoprocessor.R
+++ GIS_functions/model_geoprocessor.R
@@ -1,10 +1,5 @@
-library("hydrotools")
 library("data.table")
 library("sp")
-basepath='/var/www/R';
-source(paste(basepath,'config.R',sep='/'))
-ds = RomDataSource$new(site, rest_uname)
-ds$get_token(rest_pw)
 source(paste(hydro_tools_location,'/R/om_vahydro_metric_grid.R', sep = ''));
 
 # function to retrieve & format model segment metric & geometry data
