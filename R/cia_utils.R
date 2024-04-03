e3269c46934f66743a4d1330b54dad731a722be7
Brogan <Connor.Brogan@deq.virginia.gov>
2024-04-03 16:18:28 -0400

    Updated fn_upstream and fn_ALL.upstream to work faster through a vector approach. fn_ALL.upstream is now exported from hydrotools and offers expanded capabilities for getting entire networks back from a single data frame

--- R/cia_utils.R
+++ R/cia_utils.R
@@ -97,13 +97,13 @@ fn_river_network <- function(riv_seg, AllSegList, cia_data_frame){
   a <- 1
   cia_data <- data.frame()
   while(a <= nrow(upstream)){
-    if(upstream == 'NA'){
+    if(is.null(upstream)){
       riv_seg <- riv_seg
     }else{
       riv_seg <- upstream[a,]
     }
     #only runs code if river segment is headwater
-    if(fn_ALL.upstream(riv_seg,AllSegList) == 'NA'){
+    if(is.null(fn_ALL.upstream(riv_seg,AllSegList))){
       #determines all downstream segments
       downstream <- data.frame(fn_ALL.downstream(riv_seg, AllSegList))
       names(downstream)[names(downstream) == colnames(downstream)[1]] <- "riv_seg"
@@ -165,31 +165,30 @@ fn_river_network <- function(riv_seg, AllSegList, cia_data_frame){
 
 #extracts only the upstream functions of intended intended outlet
 #' Extracting Basin Function
-#' @description Trims data frame to include only segments upstream of end segment
-#' @param cia_data_frame Data frame of cumulative impact data
+#' @description Trims data frame to include only segments upstream of end
+#'   segment. It is inclusive of the end segment and will thus include the
+#'   outlet and all upstream segments
+#' @param cia_data_frame Data frame of cumulative impact data. This must contain
+#'   a column with the name "riverseg" that contains all river segments of
+#'   interest. This function can ONLY find upstream segments if they are present
+#'   in this riverseg column!
 #' @param end_seg Desired end river segment - river segment that is most downstream
-#' @return Trimmed cumulative impact data frame
-#' @import sqldf
+#' @return Trimmed cumulative impact data frame containing the outlet and all upstream segments
 #' @export fn_extract_basin
 fn_extract_basin <- function(cia_data_frame, end_seg){
-  #calculating upstream segments
-  upstream <- data.frame((fn_ALL.upstream(end_seg, cia_data_frame[!is.na(cia_data_frame$riverseg),]$riverseg)))
-  names(upstream)[names(upstream) == colnames(upstream)[1]] <- "riv_seg"
+  #Get all segments upstream of end_seg. Note that fn_ALL.upstream() may
+  #overwrite some data if cia_data_frame has columns used in fn_ALL.upstream()
+  upstream <- fn_ALL.upstream(riv.seg = end_seg, 
+                              AllSegList = cia_data_frame[!is.na(cia_data_frame$riverseg),],
+                              getAllSegmentsOnly = TRUE)
   
-  if(!is.data.frame(upstream)) {
-    river <- end_seg
-  } else {
-    if (nrow(upstream) == 1) {
-      if (upstream[1,] == 'NA') {
-        upstream = data.frame(riverseg = character())
-        names(upstream) <- c('riv_seg')
-      }
-    } 
-    river <- rbind(upstream,data.frame('riv_seg' = end_seg))
-  }
-  basin_data <- sqldf("SELECT * FROM river join cia_data_frame
-                    WHERE riv_seg like riverseg")
-  basin_data$riv_seg <- NULL
+  #upstream will feture a list for each segment in end_seg. Unlist for
+  #convenience of use. This will combine all upstream segments of all segments
+  #in end_seg into a single character vector
+  upstream <- unlist(upstream)
+  
+  #Get all data from the user input data frame based on the upstream segments
+  basin_data <- cia_data_frame[cia_data_frame$riverseg %in% upstream,]
   
   return(basin_data)
 }
@@ -204,101 +203,227 @@ fn_extract_basin <- function(cia_data_frame, end_seg){
 # download.file(paste(site,"/vahydro_riversegs_export",sep=""), destfile = destfile, method = "libcurl")
 # RSeg.csv <- read.csv(file=paste(localpath , filename,sep="\\"), header=TRUE, sep=",")
 # AllSegList <- substring(RSeg.csv$hydrocode, 17)
+#Newer, auth method:
+# ds <- RomDataSource$new(site, rest_uname)
+# ds$get_token(rest_pw)
+# rsegs <- ds$auth_read(uri = paste0(site,"/vahydro_riversegs_export"))
+# AllSegList <- data.frame(riverseg = gsub("vahydrosw_wshed_","",rsegs),
+#                     blankColumn = NA)
+
 ############################################################################
-
-#' Find Upstream Segments
-#' @description Trims data frame to include only segments upstream of end segment
-#' @param riv.seg string ID of segment to fine
-#' @param AllSegList AllSegList data frame of all segments
+#' Find Upstream Segments: A basic function that finds the segments immediately
+#' upstream of a given segment(s). riv.seg may be a character vector of segments.
+#' @description Returns a vector of the segments immediately upstream of riv.seg
+#'   based on the list in AllSegList
+#' @param riv.seg string ID of segment from which to find upstream segments
+#' @param AllSegList AllSegList data frame of all segments. These should be
+#'   river segments. This function can only find segments that are in this list
+#'   so the user should use a comprehensive list of segments as demonstrated in
+#'   the hydrotools package in cia_utils.R
 #' @return string next upstream segment ID
-#' @import stringr rapportools
 #' @export fn_upstream
-#fn_upstream() copy from cbp6_functions
-fn_upstream <- function(riv.seg, AllSegList) {
-  # Create dataframe for upstream and downstream segments based on code in string
-  ModelSegments <- data.frame(matrix(nrow = length(AllSegList), ncol = 6))
-  colnames(ModelSegments)<- c('RiverSeg', 'Middle', 'Last', 'AdditionalName', 'Downstream', 'Upstream')
-  ModelSegments$RiverSeg <- AllSegList
+fn_upstream <- function(AllSegList,riv.seg){
+  #Default output is NULL
+  out <- NULL
+  #First, unlist riv.seg to make it easier to work with
+  thisSeg <- unlist(riv.seg)
   
-  # Pull out 4 digit codes in middle and end for upstream/downstream segments
-  i <- 1
-  for (i in 1:nrow(ModelSegments)){
+  #If a segment is an old sub watershed, remove from the analysis as we
+  #should not guess which segments are upstream of it
+  thisSeg <- thisSeg[grepl(".+_[0-9]{4}_[0-9]{4}$",
+                           thisSeg)]
+  
+  #Get the individual segment IDs of each segment in riv.seg (i.e. excluding
+  #the segment it flows to)
+  thisSeg <- gsub(".+_([0-9]{4})_([0-9]{4}).*","\\1",thisSeg)
+  #If the end segment is defined (e.g. not NA and has length greater than
+  #zero), then get all segments upstream of all segments in riv.seg. This does
+  #NOT apply to old subwatersheds, which should not return upstream segs
+  if(all(!is.na(thisSeg)) && length(thisSeg) > 0){
+    #Combine the segments of thisSeg (which come from riv.seg) into a single
+    #or statement in regex form
+    thisSeg <- paste0("(",unlist(thisSeg),")",collapse="|")
+    #Return all river segments that have a "toSeg" column matching the ID in
+    #thisSeg. In other words, find all segments that flow into the segments in
+    #riv.seg.
+    upstreamSegs <- AllSegList$riverseg[grepl(thisSeg,AllSegList$toSeg)
+                                        & !AllSegList$oldSubshed]
     
-    ModelSegments[i,2]<- str_sub(ModelSegments[i,1], start=5L, end=8L)
-    ModelSegments[i,3]<- str_sub(ModelSegments[i,1], start=10L, end=13L)
-    ModelSegments[i,4]<- str_sub(ModelSegments[i,1], start=15L, end=-1L)
-    i <- i + 1
+    #Also capture any upstream watersheds that use the previous notation. For
+    #instance, the segment TU3_8880_9230 has several watersheds that flow into
+    #it. These include TU3_8881_8880 - TU3_8884_8880 as well as
+    #TU3_8880_9230_sf_big_cherry and TU3_8880_9230_sf_below_big_cherry. These
+    #latter two segments need to be captured
+    oldSubsheds <- AllSegList$riverseg[grepl(thisSeg,AllSegList$thisSeg) &
+                                         AllSegList$oldSubshed]
+    
+    out <- c(oldSubsheds,upstreamSegs)
+  }
+  #If no segments match a valid riv.seg input, there are no upstream segments.
+  #Return NULL instead of the empty character vector that is otherwise
+  #returned by the grepl call above. This is done for consistency of output so
+  #that any segment with no upstream segments return NULL.
+  if(length(out) == 0){
+    out <- NULL
+  }
+  #Return out from this function
+  return(out)
+}
+
+
+#' Find Upstream Segments: A basic function that finds the segments immediately
+#' upstream of a given segment(s). riv.seg may be a character vector of segments.
+#' @description Returns a vector of the segments immediately upstream of riv.seg
+#'   based on the list in AllSegList
+#' @param AllSegList Data frame that must contain riverseg and all river
+#segments in state. All columns are maintained in final This function can only
+#find segments that are in this list so the user should use a comprehensive
+#list of segments as demonstrated in the hydrotools package in cia_utils.R
+#' @param riv.seg If user wishes to only return certain segments, they can
+#specify these in riv.seg to get a data frame showing that segment, other data
+#in AllSegList, and all upstream segments. May be left as NULL to instead find
+#upstream segments of all unique segments in AllSegList
+#' @param getAllSegmentsOnly If the user only wishes to get the ids of upstream
+#segments and the outlet i.e. this is inclusive of riv.seg. This may only be
+#used if riv.seg is specified. Defaults to TRUE.
+#' @return string next upstream segment ID
+#' @export fn_ALL.upstream
+fn_ALL.upstream <- function(
+    AllSegList,
+    riv.seg = NULL,
+    getAllSegmentsOnly = TRUE
+){
+  #AllSegList should be a data frame and must contain a river segment column:
+  if(class(AllSegList) != "data.frame" || 
+     length(AllSegList$riverseg) == 0){
+    stop("The input DATA.FRAME for AllSegList must contain a column
+    with the name of \"riverseg\". This column MUST contain all river segments
+    in the area of interest (if unsure, use all river segments in the state).
+    This function can only find upstream segments that are contain in the
+    riverseg column!")
   }
   
-  # Determine Downstream Segment ----------
-  j <- 1
-  for (j in 1:nrow(ModelSegments)){
-    if (ModelSegments[j,4] != ""){
-      Downstream <- which((ModelSegments$Middle==ModelSegments$Middle[j]) & (ModelSegments$Last==ModelSegments$Last[j]) & (ModelSegments$AdditionalName==""))
-      if (length(Downstream)==0){
-        ModelSegments[j,5]  <- 'NA'
-      }else if (length(Downstream)==1){
-        ModelSegments[j,5] <- as.character(ModelSegments[Downstream,1])
-      }
-    }else if (ModelSegments[j,4]==""){
-      Downstream <- which((ModelSegments$Middle==ModelSegments$Last[j]) & (ModelSegments$AdditionalName==""))
-      if (length(Downstream)==0){
-        ModelSegments[j,5]  <- 'NA'
-      }else if (length(Downstream)==1){
-        ModelSegments[j,5] <- as.character(ModelSegments[Downstream,1])
-      }else if (length(Downstream)>1){
-        ModelSegments[j,5] <- as.character(ModelSegments[Downstream,1][1])
-      }
+  # AllSegList <- rsegs[grepl(".+_.*[1-9]+.*_.*",rsegs$riverseg),]
+  #Find the individual id of the river segment each segment flows to
+  AllSegList$toSeg <- gsub(".+_[0-9]{4}_([0-9]{4}).*","\\1",
+                           AllSegList$riverseg)
+  #Get the individual id of each river segment for reference
+  AllSegList$thisSeg <- gsub(".+_([0-9]{4})_[0-9]{4}.*","\\1",
+                             AllSegList$riverseg)
+  #Identify subwatersheds that follow the old notation of
+  #TU3_8880_9230_sf_big_cherry (which is a subwatershed of TU3_8880_9230 and
+  #must be captured as upstream of TU3_8880_9230)
+  AllSegList$oldSubshed <- grepl(".+_[0-9]{4}_[0-9]{4}_+.*",
+                                 AllSegList$riverseg)
+  
+  #Create a copy of AllSegList to alter and return to user
+  segDataFrame <- AllSegList
+  
+  #If the user has entered any values for riv.seg, set segDataFrame to only
+  #contain those segments. Maintain AllSegList for reference to use for
+  #fn_upstream()
+  if(!is.null(riv.seg) &&
+     length(riv.seg) > 0 && 
+     class(riv.seg) == 'character'
+  ){
+    #Get only river segments specified by user
+    segDataFrame <- segDataFrame[segDataFrame$riverseg %in% riv.seg,]
+  }
+  
+  #Add the next upstream segment to the data frame by calling fn_upstream() and
+  #using the riverseg column as the first input. For each river segment, this
+  #will return a character vector of all upstream segments
+  segDataFrame$fn_upstream <- mapply(fn_upstream,riv.seg = segDataFrame$riverseg,
+                                     MoreArgs = list(
+                                       AllSegList = AllSegList),
+                                     SIMPLIFY = TRUE,USE.NAMES = FALSE)
+  
+  #Now, we need to find all segments upstream of the upstream segments. These
+  #may branch into different tributaries so we will need to run fn_upstream
+  #repeatedly. Lets first create an object to store the results in. We will want
+  #to store the final list of segments in AllSegList so lets add that as a
+  #column, allUpstreamSegs
+  segDataFrame$allUpstreamSegs <- segDataFrame$fn_upstream
+  #Store upstream segments as their own vector. This will be updated in each
+  #loop iteration
+  upstreamSegs <- segDataFrame$allUpstreamSegs
+  #Define a variable that shows when there are no more upstream segments for a
+  #given river segment
+  allAreNull <- FALSE
+  #While there are values in upstreamSegs, find all river segments upstream of
+  #the values in upstreamSegs and add to allUpstreamSegs
+  while(!allAreNull) {
+    #Now get all segments upstream of the upstream segments e.g. the next
+    #segment upstream of upstreamSegs. if upstreamSegs is NULL, then there are
+    #no more upstream segments of a given river segment and fn_upstream will
+    #automatically return NULL
+    upstreamSegs <- mapply(fn_upstream,riv.seg = upstreamSegs,
+                           MoreArgs = list(
+                             AllSegList = AllSegList),
+                           SIMPLIFY = FALSE,USE.NAMES = FALSE)
+    
+    #Combine new list of next upstream segments with existing list and unlist
+    #for convenience. This gives us a vector for each river segment in
+    #AllSegList that has allUpstreamSegs so far. upstreamSegs may still have
+    #picked up on additional segments that will be searched for in the next
+    #loop. Otherwise, upstreamSegs is now NULL and will be skipped.
+    allUpstreamSegs <- mapply(list,segDataFrame$allUpstreamSegs,upstreamSegs,
+                              SIMPLIFY = FALSE,USE.NAMES = FALSE)
+    allUpstreamSegs <- mapply(unlist,allUpstreamSegs,
+                              SIMPLIFY = FALSE,USE.NAMES = FALSE)
+    
+    # #Find which lists added additional segments e.g. may have more additional
+    # #upstream segments and need to be checked again
+    # checkOut <- unlist(
+    #   mapply(identical,AllSegList$allUpstreamSegs,allUpstreamSegs,
+    #        SIMPLIFY = FALSE,USE.NAMES = FALSE)
+    # )
+    # 
+    #Store the new list of upstream river segments in allUpstreamSegs
+    segDataFrame$allUpstreamSegs <- allUpstreamSegs
+    
+    #Check if all upstream segments are NULL. This will occur when all branches
+    #of all river segments have been sorted through as a NULL is only returned
+    #when no upstream segments remain. Otherwise, upstreamSegs will have segment
+    #IDs and will need to be searched for the segments upstream of those
+    allAreNull <- mapply(is.null,upstreamSegs)
+    allAreNull <- all(allAreNull)
+  }
+  
+  #For comparison with fn_extract_basin(), add original segment into list of
+  #upstream segments and add as new column into AllSegList
+  allSegments <- mapply(list,segDataFrame$allUpstreamSegs,
+                        segDataFrame$riverseg,
+                        SIMPLIFY = FALSE,USE.NAMES = FALSE)
+  allSegments <- mapply(unlist,allSegments,
+                        SIMPLIFY = FALSE,USE.NAMES = FALSE)
+  segDataFrame$allSegments <- allSegments
+  
+  #Return all of segDataFrame from this function unless the user has specified
+  #riv.seg and that they wish to only get the watershed river segment ids
+  out <- segDataFrame
+  #If the user wishes to only get the watershed river segments, return only the
+  #last column
+  if(getAllSegmentsOnly){
+    if(length(riv.seg) == 0){
+      stop("riv.seg must contain at least 1 river segment id to use this
+      feature. When TRUE, getALLSegmentsOnly will return a list of upstream
+      river segments for only those river segments specified in riv.seg.
+      Please correct inputs.")
     }
-    j<-j+1
+    out <- segDataFrame$allSegments
+    #In the case that riv.seg is length one, convert the output list to a
+    #character vector for convenience (and to match older hydrotools function
+    #i.e. hydrotools:::fn_ALL.Upstream() and fn_upstream(), which this function
+    #is intended to replace)
+    if(length(riv.seg) == 1){
+      out <- unlist(out)
+    }
   }
-  # Determine Upstream Segment ----------
-  k<-1
-  for (k in 1:nrow(ModelSegments)){
-    Upstream <- which(as.character(ModelSegments$Downstream)==as.character(ModelSegments$RiverSeg[k]))
-    NumUp <- ModelSegments$RiverSeg[Upstream]
-    ModelSegments[k,6]<- paste(NumUp, collapse = '+')
-    if (is.empty(ModelSegments[k,6])==TRUE){
-      ModelSegments[k,6]<- 'NA'
-    } 
-    k<-k+1
-  }
-  SegUpstream <- as.numeric(which(as.character(ModelSegments$RiverSeg)==as.character(riv.seg)))
-  SegUpstream <- ModelSegments$Upstream[SegUpstream]
-  SegUpstream <- strsplit(as.character(SegUpstream), "\\+")
-  SegUpstream <- try(SegUpstream[[1]], silent = TRUE)
-  if (class(SegUpstream)=='try-error') {
-    SegUpstream <- NA
-  }
-  return(unique(SegUpstream))
+  
+  return(out)
 }
 
-
-#fn_ALL.upstream() copy from cbp6_functions
-fn_ALL.upstream <- function(riv.seg, AllSegList) {
-  UpstreamSeg <- fn_upstream(riv.seg, AllSegList)
-  AllUpstream <- character(0)
-  BranchedSegs <- character(0)
-  while (is.na(UpstreamSeg[1])==FALSE || is.empty(BranchedSegs) == FALSE) {
-    while (is.na(UpstreamSeg[1])==FALSE) {
-      num.segs <- as.numeric(length(UpstreamSeg))
-      if (num.segs > 1) {
-        BranchedSegs[(length(BranchedSegs)+1):(length(BranchedSegs)+num.segs-1)] <- UpstreamSeg[2:num.segs]
-        UpstreamSeg <- UpstreamSeg[1]
-      }
-      AllUpstream[length(AllUpstream)+1] <- UpstreamSeg
-      UpstreamSeg <- fn_upstream(UpstreamSeg, AllSegList)
-    }
-    num.branched <- as.numeric(length(BranchedSegs))
-    UpstreamSeg <- BranchedSegs[1]
-    BranchedSegs <- BranchedSegs[-1]
-  }
-  AllUpstream <- AllUpstream[which(AllUpstream != 'NA')]
-  if (is.empty(AllUpstream[1])==TRUE) {
-    AllUpstream <- 'NA'
-  }
-  return(AllUpstream)
-}
 
 
 #' Find Downstream Segment
