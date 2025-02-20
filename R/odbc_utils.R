1ab6418485d7c82ed16d5c63292e4a299327a7d5
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-02-20 12:05:47 -0500

    message

--- R/odbc_utils.R
+++ R/odbc_utils.R
@@ -23,7 +23,7 @@ fn_post_odbc <- function(entity_type, pk, inputs, con, obj=FALSE){
   if (!is.na(pk)) {
     pkid <- as.integer(as.character(inputs[[pk]]))
     if (!is.null(pkid)) {
-      message("Final na/null check")
+      message(paste("Final na/null check for pk",pk))
       if (is.na(pkid)) {
         pkid = NULL
       }
