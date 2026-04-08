eeab3037f254d38496b1f8d031c4000c5d18407a
Connor Brogan <connor.brogan@deq.virginia.gov>
2026-04-08 08:07:38 -0400

    Initialize method draft finished

--- R/WaterGageBase.R
+++ R/WaterGageBase.R
@@ -19,6 +19,10 @@ WaterGageBase <- R6Class(
     drainage_area = NA,
     #' @field gage_data The gage data from data_source
     gage_data = NA,
+    #' @field gage_data_sf A simple feature of the gage data
+    gage_data_sf = NA,
+    #'@field gage_feature a VA Hydro feature of the gage if available from ds
+    gage_feature = NA,
     #' @field statistic_id The kind of stream data to pull
     statistic_id = "00003",
     #' @field usgs_properties The fields to pull from USGS
@@ -26,23 +30,32 @@ WaterGageBase <- R6Class(
                         "time", "value", "unit_of_measure"),
     #'@low_flows Critical low flows calculated by hydrotools::xQy() via
     #'  \comp(self$comp_xQy())
-    low_flows = NA
-    
-    initialize = function(data_source, gage_id, flow_col, date_col){
+    low_flows = NA,
+    initialize = function(data_source, gage_id, flow_col, date_col,
+                          ds_in = NA, get_sf = FALSE){
       #If user is using USGS, use dataRetrieval
       if(data_source == "USGS"){
-        self$gage_id <- paste0("USGS-",gage_id)
-        self$get_gage_data()
-        
-        
-        
-        #Need to create SF of the gage, get DA, and get the watershed feature (or optional as a load_sf())?
+        if(packageVersion("dataRetrieval") >= "2.7.23") {
+          self$gage_id <- paste0("USGS-",gage_id)
+          self$get_gage_data()
+        }else{
+          #Deprecated with new dataRetrieval
+          self$get_gage_data_old()
+        }else if (is.character(data_source == "csv")){
+          self$gage_data <- read.csv(data_source)
+        }else{
+          #If user simply provides the data
+          self$gage_data <- data_source
+        }
+        if(is.na(ds_in)){
+          self$ds <- ds_in
+        }
       }
       
     },
     #new dataRetrieval
     get_gage_data = function(){
-      self$gage_data <- dataRetrieval::read_waterdata_daily(
+      gage_data_sf <- dataRetrieval::read_waterdata_daily(
         monitoring_location_id = self$gage_id, parameter_code = "00060",
         statistic_id = self$statistic_id, skipGeometry = FALSE,
         properties = c("monitoring_location_id", "parameter_code",
@@ -50,6 +63,8 @@ WaterGageBase <- R6Class(
       )
       self$flow_col <- "value"
       self$date_col <- "time"
+      
+      
     },
     #Deprecated
     get_gage_data_old = function(){
@@ -61,19 +76,44 @@ WaterGageBase <- R6Class(
       self$flow_col <- "Flow"
       self$date_col <- "Date"
     },
-    load_da = function(){
-      self$drainage_area <- NA
+    load_sf_da = function(){
+      #If user is using USGS, use dataRetrieval
+      if(data_source == "USGS"){
+        if(packageVersion("dataRetrieval") >= "2.7.23") {
+          #Need to create SF of the gage, get DA, and get the watershed feature (or optional as a load_sf())?
+          self$gage_data_sf <- dataRetrieval::read_waterdata_monitoring_location(self$gage_id)
+          self$drainage_area <- self$gage_data_sf$drainage_area
+        }else{
+          site_info <- dataRetrieval::readNWISsite(self$gage_id)
+          self$gage_data_sf <-  sf::st_as_sf(site_info, crs = 4326, 
+                                             coords = c("dec_long_va", "dec_lat_va"))
+          self$drainage_area <- site_info$drain_area_va
+        }
+        
+        if(!is.na(self$ds)){
+          self$gage_feature <- RomFeature$new(
+            ds, config = list(hydrocode = paste0("usgs_ws_",self$gage_id),
+                              bundle = "watershed",
+                              ftype = "usgs_full_drainage"),
+            TRUE)
+          if(is.na(self$gage_feature$hydroid)){
+            self$gage_feature <- NA
+            message("No feature for this watershed found")
+          }
+        }
+        
+      }else{
+        message("Please provide a USGS gage ID to use automatic geometries.")
+      }
     },
-    load_sf = function(){
-      #Need to create SF of the gage, get DA, and get the watershed feature (or optional as a load_sf())?
-    },
-    comp_xQy = function(AYS = "04-01", AYE = "03-31", startYear = NULL, endYear = NULL,
-                   x = 7, y = 10){
+    comp_xQy = function(AYS = "04-01", AYE = "03-31",
+                        startYear = NULL, endYear = NULL,
+                        x = 7, y = 10){
       self$low_flows <- xQy(
         gageDataIn = self$gage_data, flowColumn = self$flowColumn, 
         dateColumn = self$dateColumn,
         AYS = AYS, AYE = AYE, startYear = startYear,
-        endYear = endYear, x = 7, y = 10),
+        endYear = endYear, x = 7, y = 10)
       
     },
     baseflow_analysis = function(){
@@ -81,5 +121,8 @@ WaterGageBase <- R6Class(
     },
     baseflow_forecast = function(){
       
-    },
+    }
+    
+  )# End Public
+) # End R6
     
